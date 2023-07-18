de144c8beb4fc78c47cd2d5be27e37696b0f5b12
Manav Majumdar <manavmaj2001@gmail.com>
2023-07-18 10:46:17 -0400

    Added readme

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
-import ReactFlow, { addEdge, Background, Controls, MiniMap } from 'react-flow-renderer';
+import ReactFlow, { addEdge, Controls, Background } from 'react-flow-renderer';
 
 function Content() {
-  const [elements, setElements] = useState([]);
+  const [elements, setElements] = useState([
+    {
+      id: '1',
+      data: { label: 'Node 1' },
+      position: { x: 250, y: 5 },
+    }
+  ]);
+
   const onConnect = (params) => setElements((els) => addEdge(params, els));
 
   const addNode = () => {
     setElements(e => e.concat({
-      id: (e.length + 1).toString(),
-      data: { label: `Node ${e.length + 1}` },
-      position: { x: Math.random() * window.innerWidth / 2, y: Math.random() * window.innerHeight / 2 },
+      id: (elements.length + 1).toString(),
+      data: { label: `Node ${elements.length + 1}` },
+      position: { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight },
     }));
   };
 
@@ -19,15 +26,10 @@ function Content() {
       <ReactFlow
         elements={elements}
         onConnect={onConnect}
+        style={{ height: '90vh', width: '100%' }}
       >
-        <MiniMap
-          nodeColor={(n) => {
-            if (n.type === 'input') return 'blue';
-            return '#FFCC00';
-          }}
-        />
-        <Controls />
         <Background color="#aaa" gap={20} />
+        <Controls />
       </ReactFlow>
     </div>
   );
